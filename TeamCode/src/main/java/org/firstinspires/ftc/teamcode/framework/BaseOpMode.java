8df1c196851aeb2cff327e251ae2be24d93e4376
Allen B <pingguo.ny@gmail.com>
2019-11-16 20:47:17 -0500

    ocd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/BaseOpMode.java
@@ -1,22 +1,20 @@
 package org.firstinspires.ftc.teamcode.framework;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 
 import org.firstinspires.ftc.teamcode.framework.subsystems.imu.IMU;
 import org.firstinspires.ftc.teamcode.framework.subsystems.imu.BNO055;
 import org.firstinspires.ftc.teamcode.framework.enums.Corner;
+import org.firstinspires.ftc.teamcode.logging.DoubleLogger;
 
 public abstract class BaseOpMode extends LinearOpMode {
     protected DcMotor[] motors;
     protected IMU imu;
+    protected DoubleLogger logger;
 
     protected void initHardware() {
         motors = new DcMotor[]{
@@ -31,5 +29,7 @@ public abstract class BaseOpMode extends LinearOpMode {
         // Initialize imu
         BNO055IMU imuHardware = hardwareMap.get(BNO055IMU.class, "imu");
         imu = new BNO055(imuHardware);
+
+        logger = new DoubleLogger(telemetry);
     }
 }
